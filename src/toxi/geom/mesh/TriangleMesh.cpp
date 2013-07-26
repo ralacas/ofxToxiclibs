8bc4b63896d6692f89016d65e4859e3110aa08e4
Marcel Schwittlick <marzzzel@gmail.com>
2013-07-26 14:56:24 -0700

    added barebones of TriangleMesh
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ src/toxi/geom/mesh/TriangleMesh.cpp
@@ -0,0 +1,11 @@
+#include "TriangleMesh.h"
+
+
+TriangleMesh::TriangleMesh(void)
+{
+}
+
+
+TriangleMesh::~TriangleMesh(void)
+{
+}
---
+++ src/toxi/geom/mesh/TriangleMesh.h
@@ -0,0 +1,8 @@
+#pragma once
+class TriangleMesh
+{
+public:
+	TriangleMesh(void);
+	~TriangleMesh(void);
+};
+
