44d337441a92144c2a06bd3b7495d509e876cbb7
Marcel Schwittlick <marzzzel@gmail.com>
2013-07-26 14:55:59 -0700

    added barebones of Mesh3D
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ src/toxi/geom/mesh/Mesh3D.cpp
@@ -0,0 +1,11 @@
+#include "Mesh3D.h"
+
+
+Mesh3D::Mesh3D(void)
+{
+}
+
+
+Mesh3D::~Mesh3D(void)
+{
+}
---
+++ src/toxi/geom/mesh/Mesh3D.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Vec3D.h"
+#include "Vec2D.h"
+
+class Mesh3D
+{
+public:
+	Mesh3D(void);
+	~Mesh3D(void);
+	
+	virtual Mesh3D addFace(toxi::geom::Vec3D a, toxi::geom::Vec3D b, toxi::geom::Vec3D c);
+	virtual Mesh3D addFace(toxi::geom::Vec3D a, toxi::geom::Vec3D b, toxi::geom::Vec3D c, toxi::geom::Vec2D uvA, toxi::geom::Vec2D uvB,	toxi::geom::Vec2D uvC);
+	virtual Mesh3D addFace(toxi::geom::Vec3D a, toxi::geom::Vec3D b, toxi::geom::Vec3D c, toxi::geom::Vec3D n);
+	virtual Mesh3D addFace(toxi::geom::Vec3D a, toxi::geom::Vec3D b, toxi::geom::Vec3D c, toxi::geom::Vec3D n,toxi::geom:: Vec2D uvA,
+		toxi::geom::Vec2D uvB, toxi::geom::Vec2D uvC);
+};
+
