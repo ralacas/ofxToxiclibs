c8bec63adc76449e6db2c56ea394841878f3c5a6
Marcel Schwittlick <marzzzel@gmail.com>
2013-07-26 14:55:34 -0700

    add barebornes of Ray3D

---
+++ src/toxi/geom/Ray3D.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Vec3D.h"
+
+class Ray3D :
+	public toxi::geom::Vec3D
+{
+public:
+	Ray3D(void);
+	~Ray3D(void);
+
+	Vec3D getDirection( );
+	Vec3D getPointAtDistance( float d );
+};
+
