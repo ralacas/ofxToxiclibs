cbcc00a47350452b4ae5343872e3a3792d200945
Marcel Schwittlick <marzzzel@gmail.com>
2013-08-02 03:09:30 -0700

    finished implementing TriangleMesh class
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/mesh/TriangleMesh.h
+++ src/toxi/geom/mesh/TriangleMesh.h
@@ -1,16 +1,29 @@
 #pragma once
 
+#include < string >
+#include < vector >
+#include "Face.h"
+
 namespace toxi 
 {
 	namespace geom
 	{
 		namespace mesh
 		{
-			class TriangleMesh
+			class TriangleMesh : Mesh3D
 			{
 			public:
-				TriangleMesh(void);
-				~TriangleMesh(void);
+				TriangleMesh( void );
+				~TriangleMesh( void );
+				static int DEFAULT_NUM_VERTICES;
+				static int DEFAULT_NUN_FACES;
+				static int DEFAULT_STRIDE;
+
+				std::string name;
+				std::vector< Face > faces;
+
+			private:
+				
 			};
 		}
 	}
