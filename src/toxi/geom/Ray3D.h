76bc45e28ee44a366e1eed4f66a96c8be31b7868
Marcel Schwittlick <marzzzel@gmail.com>
2013-08-02 12:51:44 -0700

    continued implementing Ray3D class
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/Ray3D.h
+++ src/toxi/geom/Ray3D.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vec3D.h"
+#include < string >
 
 namespace toxi
 {
@@ -10,10 +11,18 @@ namespace toxi
 		{
 		public:
 			Ray3D(void);
+			Ray3D( float x, float y, float z, toxi::geom::Vec3D d );
+			Ray3D( toxi::geom::Vec3D o, toxi::geom::Vec3D d );
 			~Ray3D(void);
 
-			Vec3D getDirection( );
-			Vec3D getPointAtDistance( float d );
+			toxi::geom::Vec3D dir;
+
+			toxi::geom::Vec3D getDirection( );
+			toxi::geom::Vec3D getPointAtDistance( float d );
+			float getDistanceToPoint( toxi::geom::Vec3D v );
+			toxi::geom::Ray3D setDirection( toxi::geom::Vec3D v );
+			toxi::geom::Line3D toLine3DWithPointAtDistance( float dist );
+			std::string toString();
 		};
 	}
 }
