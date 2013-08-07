b25fb40b3727727c4ad99f01789b626f77472188
Marcel Schwittlick <marzzzel@gmail.com>
2013-08-06 19:58:49 -0700

    added barebones of Vec4D
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ src/toxi/geom/Vec4D.cpp
@@ -0,0 +1,11 @@
+#include "Vec4D.h"
+
+
+toxi::geom::Vec4D::Vec4D(void)
+{
+}
+
+
+toxi::geom::Vec4D::~Vec4D(void)
+{
+}
---
+++ src/toxi/geom/Vec4D.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ReadonlyVec4D.h"
+
+namespace toxi
+{
+	namespace geom
+	{
+		class Vec4D
+		{
+		public:
+			Vec4D(void);
+			virtual ~Vec4D(void);
+		};
+	}
+}
