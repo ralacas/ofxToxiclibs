e7a64239f06063bdec310384bd5739c3a3b389d7
Marcel Schwittlick <marzzzel@gmail.com>
2013-08-12 09:53:43 -0700

    changed all members to pointers in order to avoid circular dependencies
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/Axis3D.cpp
+++ src/toxi/geom/Axis3D.cpp
@@ -1,13 +1,13 @@
 #include "Axis3D.h"
 
-toxi::geom::Vec3D toxi::geom::Axis3D::X_AXIS = toxi::geom::Vec3D( 1, 0, 0 );
-toxi::geom::Vec3D toxi::geom::Axis3D::Y_AXIS = toxi::geom::Vec3D( 0, 1, 0);
-toxi::geom::Vec3D toxi::geom::Axis3D::Z_AXIS = toxi::geom::Vec3D( 0, 0, 1);
-toxi::geom::Vec3D toxi::geom::Axis3D::ZERO = toxi::geom::Vec3D( 0, 0, 0);
-toxi::geom::Vec3D toxi::geom::Axis3D::MIN_VALUE = toxi::geom::Vec3D( DBL_MIN, DBL_MIN, DBL_MIN );
-toxi::geom::Vec3D toxi::geom::Axis3D::MAX_VALUE = toxi::geom::Vec3D( DBL_MAX, DBL_MAX, DBL_MAX );
+toxi::geom::Vec3D * toxi::geom::Axis3D::X_AXIS( (1, 0, 0 ) );// = toxi::geom::Vec3D( 1, 0, 0 );
+toxi::geom::Vec3D * toxi::geom::Axis3D::Y_AXIS( ( 0, 1, 0 ) );// = toxi::geom::Vec3D( 0, 1, 0);
+toxi::geom::Vec3D * toxi::geom::Axis3D::Z_AXIS( ( 0, 0, 1 ) );// = toxi::geom::Vec3D( 0, 0, 1);
+toxi::geom::Vec3D * toxi::geom::Axis3D::ZERO( ( 0, 0, 0 ) );// = toxi::geom::Vec3D( 0, 0, 0);
+toxi::geom::Vec3D * toxi::geom::Axis3D::MIN_VALUE( ( DBL_MIN, DBL_MIN, DBL_MIN ) );// = toxi::geom::Vec3D( DBL_MIN, DBL_MIN, DBL_MIN );
+toxi::geom::Vec3D * toxi::geom::Axis3D::MAX_VALUE( ( DBL_MAX, DBL_MAX, DBL_MAX ) );// = toxi::geom::Vec3D( DBL_MAX, DBL_MAX, DBL_MAX );
 
-toxi::geom::Axis3D::Axis3D(Vec3D vec)
+toxi::geom::Axis3D::Axis3D( toxi::geom::Vec3D * vec )
 {
 	vector = vec;
 }
@@ -17,7 +17,7 @@ toxi::geom::Axis3D::~Axis3D(void)
 {
 }
 
-toxi::geom::Vec3D toxi::geom::Axis3D::getVector( void )
+toxi::geom::Vec3D * toxi::geom::Axis3D::getVector( void )
 {
 	return vector;
 }
--- src/toxi/geom/Axis3D.h
+++ src/toxi/geom/Axis3D.h
@@ -1,29 +1,27 @@
 #pragma once
 
-#include "ReadonlyVec3D.h"
-#include "Vec3D.h"
-
 namespace toxi
 {
 	namespace geom
 	{
+		class Vec3D;
 		class Axis3D
 		{
 		public:
-			Axis3D( Vec3D vec );
+			Axis3D( toxi::geom::Vec3D * vec );
 			~Axis3D(void);
 
-			Vec3D getVector( void );
+			toxi::geom::Vec3D * getVector( void );
 
-			static Vec3D X_AXIS;
-			static Vec3D Y_AXIS;
-			static Vec3D Z_AXIS;
-			static Vec3D ZERO;
-			static Vec3D MIN_VALUE;
-			static Vec3D MAX_VALUE;
+			static toxi::geom::Vec3D * X_AXIS;
+			static toxi::geom::Vec3D * Y_AXIS;
+			static toxi::geom::Vec3D * Z_AXIS;
+			static toxi::geom::Vec3D * ZERO;
+			static toxi::geom::Vec3D * MIN_VALUE;
+			static toxi::geom::Vec3D * MAX_VALUE;
 
 		private: 
-			Vec3D vector;
+			toxi::geom::Vec3D * vector;
 		};
 	}
 }
