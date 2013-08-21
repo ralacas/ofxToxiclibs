a3b7c64e470fd74f4799daf3a92697326ce2022d
Marcel Schwittlick <marzzzel@gmail.com>
2013-08-21 09:28:08 -0700

    implemented Face class
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/mesh/Face.cpp
+++ src/toxi/geom/mesh/Face.cpp
@@ -3,15 +3,81 @@
 
 toxi::geom::mesh::Face::Face(void)
 {
-	//TODO implement this
+	this->a = 0;
+	this->b = 0;
+	this->c = 0;
+	normal = new toxi::geom::Vec3D();
+	a->addFaceNormal(normal);
+	b->addFaceNormal(normal);
+	c->addFaceNormal(normal);
 }
 
+toxi::geom::mesh::Face::Face( Vertex * a, Vertex * b, Vertex * c )
+{
+	this->a = a;
+	this->b = b;
+	this->c = c;
+	normal = &a->sub(c).crossSelf(&a->sub(b)).normalize();
+	a->addFaceNormal(normal);
+	b->addFaceNormal(normal);
+	c->addFaceNormal(normal);
+}
+
+toxi::geom::mesh::Face::Face( Vertex * a, Vertex * b, Vertex * c, Vec2D * uvA, Vec2D * uvB, Vec2D * uvC )
+{
+	this->a = a;
+	this->b = b;
+	this->c = c;
+	normal = &a->sub(c).crossSelf(&a->sub(b)).normalize();
+	a->addFaceNormal(normal);
+	b->addFaceNormal(normal);
+	c->addFaceNormal(normal);
+
+	this->uvA = uvA;
+	this->uvB = uvB;
+	this->uvC = uvC;
+}
 
 toxi::geom::mesh::Face::~Face(void)
 {
+	delete normal;
 }
 
-toxi::geom::Triangle3D toxi::geom::mesh::Face::toTriangle()
+toxi::geom::Triangle3D * toxi::geom::mesh::Face::toTriangle()
 {
-	return toxi::geom::Triangle3D( *a, *b, *c );
+	return new toxi::geom::Triangle3D( *a, *b, *c );
+}
+
+void toxi::geom::mesh::Face::computeNormal()
+{
+	normal = &a->sub(c).crossSelf(&a->sub(b)).normalize();
+}
+
+void toxi::geom::mesh::Face::flipVertexOrder()
+{
+	Vertex * t = a;
+	a = b;
+	b = t;
+	normal->invert();
+	delete t;
+}
+
+toxi::geom::Vec3D * toxi::geom::mesh::Face::getCentroid()
+{
+	return &a->add(b)->addSelf(c).scale(1.0 / 3);
+}
+
+std::vector< toxi::geom::mesh::Vertex * >  toxi::geom::mesh::Face::getVertices()
+{
+	std::vector< toxi::geom::mesh::Vertex *>  verts;
+	verts.push_back( a );
+	verts.push_back( b );
+	verts.push_back( c );
+	return verts;
+}
+
+std::string toxi::geom::mesh::Face::toString()
+{
+	std::stringstream ss;
+	ss << "toxi::geom::mesh::Face: a:" << a->toString() << " b:" << b->toString() << " c:" << c->toString();
 }
--- src/toxi/geom/mesh/Face.h
+++ src/toxi/geom/mesh/Face.h
@@ -6,34 +6,34 @@
 #include < vector >
 #include < string >
 
+#include "../Vec3D.h"
+#include "../Triangle3D.h"
+
 namespace toxi
 {
 	namespace geom
 	{
 		namespace mesh
 		{
-			class Vertex;
-			class Triangle3D;
-			class Vec2D;
-			class Vec3D;
+
 			class Face
 			{
 			public:
 				Face( void );
-				Face( Vertex a, Vertex b, Vertex c );
-				Face( Vertex a, Vertex b, Vertex c, Vec2D uvA, Vec2D uvB, Vec2D uvC );
+				Face( Vertex * a, Vertex * b, Vertex * c );
+				Face( Vertex * a, Vertex * b, Vertex * c, Vec2D * uvA, Vec2D * uvB, Vec2D * uvC );
 				~Face(void);
 
 				toxi::geom::mesh::Vertex *a, *b, *c;
-				toxi::geom::Vec2D uvA, uvB, uvC;
-				toxi::geom::Vec3D normal;
+				toxi::geom::Vec2D * uvA, * uvB, * uvC;
+				toxi::geom::Vec3D * normal;
 
 				void computeNormal();
 				void flipVertexOrder();
-				toxi::geom::Vec3D getCentroid( );
-				Vertex* getVertices( );
+				toxi::geom::Vec3D * getCentroid( );
+				std::vector< Vertex * >  getVertices( );
 				std::string toString( );
-				toxi::geom::Triangle3D toTriangle( );
+				toxi::geom::Triangle3D * toTriangle( );
 				
 			};
 		}
