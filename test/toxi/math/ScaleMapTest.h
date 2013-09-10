e121e5b41c92a6d6d455385bd3e1c5c8bdec60dd
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-10 05:56:03 -0700

    finished ScaleMap tests
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- test/toxi/math/ScaleMapTest.h
+++ test/toxi/math/ScaleMapTest.h
@@ -10,10 +10,22 @@ class ScaleMapTest : public CppUnit::TestFixture
 {
 public:
 	CPPUNIT_TEST_SUITE( ScaleMapTest );
-	//CPPUNIT_TEST( interpolate );
+	CPPUNIT_TEST( constructorTest );
+	CPPUNIT_TEST( assignmentOperatorOverloading );
+	CPPUNIT_TEST( clippedValue );
+	CPPUNIT_TEST( median );
+	CPPUNIT_TEST( getMappedValueFor );
+	CPPUNIT_TEST( getMappedMedian );
+	CPPUNIT_TEST( gettersAndSetters );
 	CPPUNIT_TEST_SUITE_END();
 
-
+	void constructorTest( void );
+	void assignmentOperatorOverloading( void );
+	void clippedValue( void );
+	void median( void );
+	void getMappedValueFor( void );
+	void getMappedMedian( void );
+	void gettersAndSetters( void );
 };
 
 #endif // !__SCALE_MAP_TEST_H__
