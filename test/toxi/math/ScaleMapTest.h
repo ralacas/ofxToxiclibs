4b0029dd837231636a2787781053ee7e1856540a
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-10 01:06:40 -0700

    add ScaleMapTest barebones
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ test/toxi/math/ScaleMapTest.h
@@ -0,0 +1,19 @@
+#ifndef __SCALE_MAP_TEST_H__
+#define __SCALE_MAP_TEST_H__
+
+#pragma once
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCase.h>
+
+class ScaleMapTest : public CppUnit::TestFixture
+{
+public:
+	CPPUNIT_TEST_SUITE( ScaleMapTest );
+	//CPPUNIT_TEST( interpolate );
+	CPPUNIT_TEST_SUITE_END();
+
+
+};
+
+#endif // !__SCALE_MAP_TEST_H__
