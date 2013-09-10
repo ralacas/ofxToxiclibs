5364355144d7914ac9cd294d56cc0d60c7ace661
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-10 00:29:12 -0700

    added toxiclibs object for static initialization of random seed and other things that are gonna be added in the future
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ src/toxi/main/Toxiclibs.cpp
@@ -0,0 +1,17 @@
+#include "Toxiclibs.h"
+
+
+Toxiclibs::Toxiclibs(void)
+{
+}
+
+
+Toxiclibs::~Toxiclibs(void)
+{
+}
+
+void Toxiclibs::init( void )
+{
+	std::cout << "Initialized Toxiclibs!" << std::endl;
+	std::srand( time( NULL ) );
+}
---
+++ src/toxi/main/Toxiclibs.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef __TOXICLIBS_H__
+#define __TOXICLIBS_H__
+
+
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
+
+class Toxiclibs
+{
+public:
+	Toxiclibs(void);
+	~Toxiclibs(void);
+
+	static void init( void );
+};
+
+#endif
