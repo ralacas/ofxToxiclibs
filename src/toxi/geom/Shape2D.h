085ccdbad2c7baed445301e35543d7c50f65d9ee
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-10 23:43:34 -0700

    add Shape2D interface
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ src/toxi/geom/Shape2D.h
@@ -0,0 +1,31 @@
+#ifndef __SHAPE_2D_H__
+#define __SHAPE_2D_H__
+
+#pragma once
+
+//#include "Circle.h"
+//#include "Polygon2D.h"
+
+namespace toxi
+{
+	namespace geom
+	{
+		class Vec2D;
+		class Rect;
+		class Circle;
+		class Polygon2D;
+		class Shape2D
+		{
+		public:
+			virtual bool containsPoint( toxi::geom::Vec2D * p ) = 0;
+			virtual float getArea( ) = 0;
+			virtual toxi::geom::Circle * getBoundingCircle() = 0;
+			virtual toxi::geom::Rect getBounds() = 0;
+			virtual float getCircumference() = 0;
+			virtual toxi::geom::Vec2D getRandomPoint() = 0;
+			virtual toxi::geom::Polygon2D toPolygon2D() = 0;
+		};
+	}
+}
+
+#endif
