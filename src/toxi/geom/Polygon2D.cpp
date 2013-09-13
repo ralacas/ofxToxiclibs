a6fb5a91d4e62dc2b1d14f0af4f87e6a59c5305f
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-13 06:07:07 -0700

    add getEdges() function
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/Polygon2D.cpp
+++ src/toxi/geom/Polygon2D.cpp
@@ -1,4 +1,6 @@
 #include "Polygon2D.h"
+#include <vector>
+#include "Line2D.h"
 
 
 toxi::geom::Polygon2D::Polygon2D(void)
@@ -18,4 +20,15 @@ toxi::geom::Polygon2D toxi::geom::Polygon2D::add( float x, float y )
 toxi::geom::Polygon2D toxi::geom::Polygon2D::add( toxi::geom::Vec2D * v )
 {
 	return *this;
+}
+
+std::vector< toxi::geom::Line2D * > toxi::geom::Polygon2D::getEdges()
+{
+	int num = vertices.size();
+	std::vector< Line2D * > edges;
+	for( int i = 0; i < num; i++ )
+	{
+		int i2 = std::fmod( ( i + 1 ), num );
+		edges.push_back( new Line2D( vertices[i], vertices[ i2 ] ) );
+	}
 }
--- src/toxi/geom/Polygon2D.h
+++ src/toxi/geom/Polygon2D.h
@@ -4,20 +4,25 @@
 #pragma once
 
 //#include "Vec2D.h"
+#include <vector>
 
 namespace toxi
 {
 	namespace geom
 	{
 		class Vec2D;
+		class Line2D;
 		class Polygon2D
 		{
 		public:
 			Polygon2D(void);
 			~Polygon2D(void);
 
+			std::vector< Vec2D * > vertices;
+
 			Polygon2D add( float x, float y );
 			Polygon2D add( toxi::geom::Vec2D * v );
+			std::vector< Line2D * > getEdges();
 		};
 
 	}
