d2361deda87ffe7f6e9959224baa8218791e306e
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-19 07:35:30 -0700

    implements - doubleToLongBits( double d ) and - floatToIntBits( float f )
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/VecMathUtil.cpp
+++ src/toxi/geom/VecMathUtil.cpp
@@ -1,6 +1,5 @@
 #include "VecMathUtil.h"
 
-
 toxi::geom::VecMathUtil::VecMathUtil(void)
 {
 }
@@ -12,12 +11,38 @@ toxi::geom::VecMathUtil::~VecMathUtil(void)
 
 long toxi::geom::VecMathUtil::doubleToLongBits( double d )
 {
-	//TODO: tinker this out
-	return 1;
+	if( d == 0.0 ) {
+		return 0L;
+	}
+	else
+	{
+		union types {
+			double dou;
+			long lo;
+		};
+
+		types t;
+		t.dou = d;
+		long returnLong = t.lo;
+		return returnLong;
+	}
 }
 
 int toxi::geom::VecMathUtil::floatToIntBits( float f )
 {
-	//TODO: tinker this out too.
-	return 1;
+	if( f == 0.0 )
+	{
+		return 0;
+	}
+	else
+	{
+		union types {
+			float fl;
+			int in;
+		};
+		types t;
+		t.fl = f;
+		int returnInt = t.in;
+		return returnInt;
+	}
 }
