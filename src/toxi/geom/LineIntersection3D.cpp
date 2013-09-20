aff5db1e4dfa3908e4f94abb258e9d698588feef
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-20 11:51:23 -0700

    added LineIntersection3D for the use with Line3D
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ src/toxi/geom/LineIntersection3D.cpp
@@ -0,0 +1,20 @@
+#include "LineIntersection3D.h"
+#include "Line3D.h"
+
+
+toxi::geom::LineIntersection3D::LineIntersection3D(void)
+{
+}
+toxi::geom::LineIntersection3D::LineIntersection3D( Type type )
+{
+
+}
+
+toxi::geom::LineIntersection3D::LineIntersection3D( Type type, Line3D line, float mua, float mub )
+{
+
+}
+
+toxi::geom::LineIntersection3D::~LineIntersection3D(void)
+{
+}
