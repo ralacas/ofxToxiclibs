9b89c7470be14fb449b2901ea2be457ec354b0b1
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-20 11:54:28 -0700

    refactoring
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/mesh/Mesh3D.h
+++ src/toxi/geom/mesh/Mesh3D.h
@@ -16,7 +16,7 @@ namespace toxi
 		namespace mesh
 		{
 			class Sphere;
-			class AABB;
+			//class AABB;
 			class Vec2D;
 			class Vertex;
 			class Face;
@@ -33,17 +33,17 @@ namespace toxi
 				virtual Mesh3D* addFace(toxi::geom::Vec3D * a, toxi::geom::Vec3D * b, toxi::geom::Vec3D * c, toxi::geom::Vec3D * n, toxi::geom::Vec2D * uvA,
 					toxi::geom::Vec2D * uvB, toxi::geom::Vec2D * uvC) = 0;
 				virtual Mesh3D* addMesh( Mesh3D* m ) = 0;
-				virtual AABB center( Vec3D origin ) = 0;
+				virtual toxi::geom::AABB center( toxi::geom::Vec3D origin ) = 0;
 				virtual Mesh3D* clear() = 0;
-				virtual Vec3D computeCentroid() = 0;
+				virtual toxi::geom::Vec3D * computeCentroid() = 0;
 				virtual Mesh3D* computeFaceNormals() = 0;
 				virtual Mesh3D* computeVertexNormals() = 0;
 				virtual Mesh3D* faceOutwards() = 0;
 				virtual Mesh3D* flipVertexOrder() = 0;
 				virtual Mesh3D* flipYAxis() = 0;
-				virtual AABB * getBoundingBox() = 0;
-				virtual Sphere * getBoundingSphere() = 0;
-				virtual Vertex * getClosestVertexToPoint( Vec3D * p ) = 0;
+				virtual toxi::geom::AABB * getBoundingBox() = 0;
+				//virtual toxi::geom::Sphere * getBoundingSphere() = 0;
+				virtual Vertex * getClosestVertexToPoint( toxi::geom::Vec3D * p ) = 0;
 				virtual std::vector< toxi::geom::mesh::Face > getFaces() = 0;
 				virtual int getNumFaces() = 0;
 				virtual int getNumVertices()  = 0;
