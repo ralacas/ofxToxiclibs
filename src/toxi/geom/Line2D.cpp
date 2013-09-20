74c56d17342748fe90c528b097172da19da5a1d9
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-20 11:52:36 -0700

    continues implementing Line2D
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/geom/Line2D.cpp
+++ src/toxi/geom/Line2D.cpp
@@ -36,3 +36,54 @@ toxi::geom::Vec2D * toxi::geom::Line2D::closestPointTo( toxi::geom::Vec2D p )
 
 	return a->add( v->scaleSelf( t ) );
 }
+
+toxi::geom::LineIntersection * toxi::geom::Line2D::intersectLine( Line2D * l )
+{
+	LineIntersection * isec = nullptr;
+	float denom = (l->b->getY() - l->a->getY()) * (b->getX() - a->getX()) - (l->b->getX() - l->a->getX())
+		* ( b->getY() - a->getY() );
+
+	float na = (l->b->getX() - l->a->getX()) * (a->getY() - l->a->getY()) - (l->b->getY() - l->a->getY())
+		* (a->getX() - l->a->getX());
+	float nb = (b->getX() - a->getX()) * (a->getY() - l->a->getY()) - (b->getY() - a->getY()) * (a->getX() - l->a->getX());
+
+	if (denom != 0.0) {
+		float ua = na / denom;
+		float ub = nb / denom;
+		Vec2D * i = a->interpolateTo(b, ua);
+		if (ua >= 0.0f && ua <= 1.0 && ub >= 0.0 && ub <= 1.0) {
+			isec = new LineIntersection(toxi::geom::LineIntersection::Type::INTERSECTING, *i, ua, ub);
+		} else {
+			isec = new LineIntersection(toxi::geom::LineIntersection::Type::NON_INTERSECTING, *i, ua, ub);
+		}
+	} else {
+		if (na == 0.0 && nb == 0.0) {
+			if (distanceToPoint(l->a) == 0.0) {
+				isec = new LineIntersection(toxi::geom::LineIntersection::Type::COINCIDENT, nullptr);
+			} else {
+				isec = new LineIntersection(toxi::geom::LineIntersection::Type::COINCIDENT_NO_INTERSECT,
+					nullptr);
+			}
+		} else {
+			isec = new LineIntersection(toxi::geom::LineIntersection::Type::PARALLEL, nullptr);
+		}
+	}
+	return isec;
+}
+
+std::string toxi::geom::Line2D::toString()
+{
+	std::stringstream ss;
+	ss << a->toString() << " -> " << b->toString();
+	return ss.str();
+}
+
+float toxi::geom::Line2D::getLength()
+{
+	return a->distanceTo( b );
+}
+
+float toxi::geom::Line2D::distanceToPoint( Vec2D * p )
+{
+	return closestPointTo( *p )->distanceTo( p );
+}
--- src/toxi/geom/Line2D.h
+++ src/toxi/geom/Line2D.h
@@ -4,6 +4,8 @@
 #pragma once
 
 #include "Vec2D.h"
+#include "LineIntersection.h"
+#include <vector>
 
 namespace toxi
 {
@@ -15,12 +17,21 @@ namespace toxi
 		public:
 			Line2D(void);
 			Line2D( Vec2D * v1, Vec2D * v2 );
-			Line2D( float x1, float y1, float x2, float y2 );
+			Line2D( float x1, float y1, float x2, float y2 );	
 			~Line2D(void);
+
+			inline bool operator == (const Line2D & lhs)
+			{
+				return ( *lhs.a == *this->a && *lhs.b == *this->b );
+			}
 
 			toxi::geom::Vec2D * a, * b;
 
 			Vec2D * closestPointTo( Vec2D v );
+			toxi::geom::LineIntersection * intersectLine( Line2D * l );
+			float distanceToPoint( Vec2D * p );
+			std::string toString();
+			float getLength();
 		};
 
 
