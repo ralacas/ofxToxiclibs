c72e160ebf6f5b1f3a26733617a34a00218f1ec9
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-22 09:47:42 -0700

    adds Random class
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

---
+++ src/toxi/util/Random.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef __RANDOM_H__
+#define __RANDOM_H__
+
+#include <random>
+#include <algorithm>
+
+namespace toxi
+{
+	namespace util
+	{
+		class Random
+		{
+
+		private:
+			std::random_device rnd_dev;
+			//std::default_random_engine dre;
+
+		public:
+			Random( void );
+			~Random(void);
+
+			static double getRandom( double min, double max );
+			static long getRandom( long min, long max );
+		};
+	}
+}
+
+#endif
