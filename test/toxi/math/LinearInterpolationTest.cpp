4ebd418ce55aa36da10864eff8b8f25b3a3259db
Marcel Schwittlick <marzzzel@gmail.com>
2013-09-23 08:56:17 -0700

    refactors some use of classes in LinearInterpolationTest
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- test/toxi/math/LinearInterpolationTest.cpp
+++ test/toxi/math/LinearInterpolationTest.cpp
@@ -4,14 +4,12 @@
 void LinearInterpolationTest::interpolate( void )
 {
 	// double
-	toxi::math::LinearInterpolation * stat = new toxi::math::LinearInterpolation();
-	double val = stat->interpolate(0.0, 10.0, 0.5 );
+	toxi::math::LinearInterpolation stat = toxi::math::LinearInterpolation();
+	double val = stat.interpolate(0.0, 10.0, 0.5 );
 	bool r1 = ( val == 5.0) ? true : false;
 
 	//float
-	float val2 = stat->interpolate(0.0f, 100.0f, 0.5f );
+	float val2 = stat.interpolate(0.0f, 100.0f, 0.5f );
 	bool r2 = val2 == 50.0 ? true : false;
 	CPPUNIT_ASSERT( r1 && r2 );
-
-	delete stat;
 }
