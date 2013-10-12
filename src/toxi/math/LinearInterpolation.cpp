66e7752e3c929a04ef421b0b5cf39de737150f63
Marcel Schwittlick <marzzzel@gmail.com>
2013-10-13 00:33:45 +0200

    change function head
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/toxi/math/LinearInterpolation.cpp
+++ src/toxi/math/LinearInterpolation.cpp
@@ -10,12 +10,14 @@ toxi::math::LinearInterpolation::~LinearInterpolation(void)
 {
 }
 
-double toxi::math::LinearInterpolation::interpolate( double a, double b, double f )
+double toxi::math::LinearInterpolation::interpolate( const double & a, const double & b, const double & f )
 {
+	//std::cout << "Linear Inpolated" << std::endl;
 	return a + (b - a) * f;
 }
 
-float toxi::math::LinearInterpolation::interpolate( float a, float b, float f )
+float toxi::math::LinearInterpolation::interpolate( const float & a, const float & b, const float & f )
 {
+	//std::cout << "Linear Inpolated" << std::endl;
 	return a + (b - a) * f;
 }
--- src/toxi/math/LinearInterpolation.h
+++ src/toxi/math/LinearInterpolation.h
@@ -4,6 +4,7 @@
 #define __LINEAR_INTERPOLATION_H__
 
 #include "InterpolateStrategy.h"
+#include <stdio.h>
 
 namespace toxi
 {
@@ -15,8 +16,8 @@ namespace toxi
 				LinearInterpolation(void);
 				~LinearInterpolation(void);
 
-				double interpolate(double a, double b, double f);
-				float interpolate(float a, float b, float f);
+				double interpolate(const double & a, const double & b, const double & f); 
+				float interpolate(const float & a, const float & b, const float & f);
 		};
 	}
 }
